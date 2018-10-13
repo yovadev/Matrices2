d10ad45920f05b4096692dea17ed56401b3f275b
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:41:03 -0600

    Initial commit

---
+++ src/matrices2/Matrices2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrices2;
+
+/**
+ *
+ * @author Yova
+ */
+public class Matrices2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre[] = {"valor1", "valor2"};
+        System.out.println(nombre[0]);
+    }
+    
+}
